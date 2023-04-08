13ff742d55466e07981f2a3b6b5de395f35b2ae9
PhiriFrank <125193743+PhiriFrank@users.noreply.github.com>
2023-04-08 15:38:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,14 +4,14 @@
 #include <iostream>
 #include<Cmath>
 bool checkPrime(uint64_t value) {
-    for (uint64_t i = 3; i <= sqrt(value); i++) {
+    for (uint64_t i = 2; i <= sqrt(value); i++) {
         if (value % i == 0)
             return false;
     }
     return true;
 }
 uint64_t nPrime(uint64_t n) {
-    uint64_t curNum = 3;
+    uint64_t culNum = 2;
     uint64_t i = 0;
     while (1) {
         if (checkPrime(curNum) == 1) {
@@ -20,10 +20,10 @@ uint64_t nPrime(uint64_t n) {
         if (i == n) {
             break;
         }
-        ++curNum;
+        ++culNum;
 
     }
-    return curNum;
+    return culNum;
 
 }
 uint64_t nextPrime(uint64_t value) {
@@ -39,15 +39,15 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t curNum= 3;
+    uint64_t curNum= 2;
     uint64_t sum = 0;
-    while (curNum<hbound) {
+    while (culNum<hbound) {
         if (checkPrime(curNum)) {
             sum += curNum;
             curNum++;
         }
         else {
-            curNum++;
+            culNum++;
         }
         
     }
